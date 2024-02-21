84743160534cce724284f903e0a6994e716f4dc6
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2024-02-21 13:01:58 -0600

    CSS to basic structure, responsive menu

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,33 +1,59 @@
-import Link from 'next/link'
+"use client";
+import { useState } from 'react';
+import NavLink from './navLink/navLink';
+import styles from './links.module.css'
 
+const links = [
+    {
+        title: 'Home',
+        path: '/'
+    },
+    {
+        title: 'Contact',
+        path: '/contact'
+    },
+    {
+        title: 'About',
+        path: '/about'
+    },
+    {
+        title: 'Blog',
+        path: '/blog'
+    }
+];
 const Links = () => {   
 
-    const links = [
-        {
-            title: 'Home',
-            path: '/'
-        },
-        {
-            title: 'Contact',
-            path: '/contact'
-        },
-        {
-            title: 'About',
-            path: '/about'
-        },
-        {
-            title: 'Blog',
-            path: '/blog'
-        }
-    ];
+    const [open, setOpen] = useState(false);
+
+    // TEMPORARY
+    const session = true;
+    const isAdmin = true;
 
     return (
-        <div>
-            {links.map((link => (
-                <Link href={link.path} key={link.title}>
-                    {link.title}
-                </Link>
-            )))}
+        <div className={styles.container}>
+            <div className={styles.links}>
+                {links.map((link) => (
+                    <NavLink item={link} key={link.title} />
+                ))}
+                {session ? (
+                    <>
+                    {
+                        isAdmin && <NavLink item={{title: 'Admin', path: '/admin'}} />}
+                        <button className={styles.logout}>Logout</button>
+                    </>
+                ) : (
+                    <NavLink item={{title: 'Login', path: '/login'}} />
+                )
+                }
+            </div>
+            <button className={styles.menuButton} onClick={() => setOpen(!open)}>Menu</button>
+            {
+                open && <div className={styles.mobileLinks}>
+                    {links.map((link => (
+                        <NavLink item={link} key={link.title} />
+                    )))}
+                </div>
+            }
         </div>
     )
   };

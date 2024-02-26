2dd466c17ff77528809ef976ebf6d8ef35fc1924
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2024-02-26 11:51:58 -0600

    About page finished

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,14 +1,38 @@
 import Image from 'next/image';
-// import styles from './about.module.css';
+import styles from './about.module.css';
 
 const AboutPage = () => {
-    return (
-        <div>
-            {/* <div className={styles.imgContainer}> */}
-            <Image src="/about.png" alt="" width={500} height={500} />
-            {/* </div> */}
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>We create digital ideas thar are bigger, bolder, braver and better.</h1>
+        <p className={styles.description}>
+          We create digital ideas thar are bigger, bolder, braver and better.
+          We believe in good ideas flexibility and precision.
+          Our Special Team best consulting & finance solution provider.
+          Wide range of web and software development services.
+        </p>
+        <div className={styles.boxes}>
+            <div className={styles.box}>
+                <h1>10 K+</h1>
+                <p>Years of Experience</p>
+            </div>
+            <div className={styles.box}>
+                <h1>234 K+</h1>
+                <p>People reached</p>
+            </div>
+            <div className={styles.box}>
+                <h1>5 K+</h1>
+                <p>Services and plugins</p>
+            </div>
         </div>
-    )
-  };
-  
-  export default AboutPage;
+      </div>
+      <div className={styles.imgContainer}>
+          <Image src="/about.png" alt="" fill className={styles.img}/>
+      </div>
+    </div>
+  )
+};
+
+export default AboutPage;
